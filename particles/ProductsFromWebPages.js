2481fb18d6146a4b1fc942cdab63343b0cca8176
Matt Small <mbsmall@google.com>
2017-08-30 14:21:37 -0700

    an initial (non-working) version

---
+++ particles/ProductsFromWebPages.js
@@ -0,0 +1,22 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+"use strict";
+
+defineParticle(({Particle}) => {
+  return class ProductsFromWebPages extends Particle {
+    setViews(views) {
+      console.log('ProductsFromWebPages setViews');
+      this.on(views, 'list', 'change', e => {
+        console.log('ProductsFromWebPages setViews.on callback');
+        let webPages = views.get('list');
+        // this.logDebug('webPages', webPages);
+      });
+    }
+  }
+});
